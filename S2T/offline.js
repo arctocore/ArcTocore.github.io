8a122faef35981b2e7fac30602aecd453cb2b008
xxx <xxx>
2020-08-04 11:58:11 +0200

    init
    init

---
+++ S2T/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1590167861,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/denmarkbtn-sheet0.png",
+		"images/englishbtn-sheet0.png",
+		"images/germanbtn-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
---
+++ T2S/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1590167662,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/germansprite-sheet0.png",
+		"images/englishsprite-sheet0.png",
+		"images/francesprite-sheet0.png",
+		"images/spainsprite-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/flash-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
